8f73f0238d29dbedb8750f7c7950a8e96f391e7b
cyia <demiancy.dev@gmail.com>
2022-02-14 21:42:54 -0300

    Cryptocurrencies page

--- app/src/components/Homepage.jsx
+++ app/src/components/Homepage.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import millify from 'millify';
+import { Link } from 'react-router-dom';
 import { 
   Row, 
+  Col,
   Typography, 
-  Col, 
   Statistic 
 } from 'antd';
-import { Link } from 'react-router-dom';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '../components'
 
 const { Title } = Typography;
 
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
 
   if (isFetching) return 'Loading...';
@@ -40,6 +41,26 @@ const Homepage = () => {
           <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} />
         </Col>
       </Row>
+
+      <div className='home-heading-container'>
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className="show-more">
+           <Link to="/cryptocurrencies">Show more</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+
+      <div className='home-heading-container'>
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+           <Link to="/news">Show more</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   )
 }
