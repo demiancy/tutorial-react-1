e567abb1eaad9ce66292f3543f4a574584651a7b
cyia <demiancy.dev@gmail.com>
2022-02-20 22:18:10 -0300

    Added the chart line on crypto detail

--- app/src/components/LineChart.jsx
+++ app/src/components/LineChart.jsx
@@ -6,28 +6,76 @@ import {
   Typography
 } from 'antd';
 
-const { Title } = Typography;
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
 
+  const coinPrice     = [];
+  const coinTimestamp = [];
 
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinPrice.push(coinHistory.data.history[i].price);
+    coinTimestamp.push(new Date(coinHistory.data.history[i].timestamp).toLocaleDateString());
+  }
+
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: 'Price In USD',
+        data: coinPrice,
+        fill: false,
+        backgroundColor: '#0071bd',
+        borderColor: '#0071bd',
+      },
+    ],
+  };
+
+  const options = { 
+    scales: { 
+      y: { 
+        ticks: { 
+          beginAtZero: true, 
+        }, 
+      }, 
+    }, 
+  };
 
   return (
     <>
       <Row className="chart-header">
-        <Title level={2} className="chart-title">
+        <Typography.Title level={2} className="chart-title">
           {coinName} Price Chart
-        </Title>
+        </Typography.Title>
         <Col className="price-container">
-          <Title level={5} className="price-change">
+          <Typography.Title level={5} className="price-change">
             Change: {coinHistory?.data?.change}%
-          </Title>
-          <Title level={5} className="current-price">
+          </Typography.Title>
+          <Typography.Title level={5} className="current-price">
             Current {coinName} Price: $ {currentPrice}
-          </Title>
+          </Typography.Title>
         </Col>
       </Row>
- 
+      <Line data={data} options={options} />
     </>
   )
 }
